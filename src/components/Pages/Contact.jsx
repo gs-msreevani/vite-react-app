fbcc75f9cb7b18393e3eb8ce3982fe97d4b158d8
gs-msreevani <msreevani@gainsight.com>
2024-09-10 11:55:19 +0530

    adding buttons in conatct page

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,6 +1,12 @@
 const Contact = ()=>{
     return (
-        <h3>This is the contact page</h3>
+        <>
+            <h3>This is the contact page</h3>
+            <button id='GM'>GM</button>
+            <button id='SM'>SM</button>
+            <button id='FM'>FM</button>
+        </>
+
     )
 }
 export default Contact;
