2d3f1877bf168913f2fb1cc9806e1c9891deefb9
gs-msreevani <msreevani@gainsight.com>
2024-09-11 13:51:48 +0530

    adding custom events

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,10 +1,23 @@
+import {useState,useEffect} from "react";
+
 const Contact = ()=>{
+    const [roles,setRoles] = useState([]);
+    useEffect(()=>{
+        window.aptrinsic('track','roles',{"roles":roles});
+        console.log(roles);
+    },[roles])
+    const handleRole = (role)=>{
+        if(!roles.includes(role)){
+            setRoles((prevRoles)=>[...prevRoles,role]);
+        }
+        console.log(roles);
+    }
     return (
         <>
             <h3>This is the contact page</h3>
-            <button id='GM'>GM</button>
-            <button id='SM'>SM</button>
-            <button id='FM'>FM</button>
+            <button id='GM' onClick={()=>handleRole("GM")}>GM</button>
+            <button id='SM' onClick={()=>handleRole("SM")}>SM</button>
+            <button id='FM' onClick={()=>handleRole("FM")}>FM</button>
         </>
 
     )
