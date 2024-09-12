ce7af1d711f1ada2045729e7c83be1ac81e5362d
gs-msreevani <msreevani@gainsight.com>
2024-09-12 14:39:42 +0530

    adding place order button

--- src/components/Pages/SingleProduct.jsx
+++ src/components/Pages/SingleProduct.jsx
@@ -2,9 +2,15 @@ import {useParams} from "react-router-dom";
 
 const SingleProduct = ()=>{
     const {id} = useParams();
+    const placeOrder = ()=>{
+        window.aptrinsic("track","placed",{"placed":"yes"});
+    }
     return(
-        <h2>This is Single Product - {id} </h2>
+        <>
+            <h2>This is Single Product - {id} </h2>
+            <button onClick={placeOrder}>Place Order</button>
+        </>
     )
 }
 
-export  default  SingleProduct;
+export default SingleProduct;
